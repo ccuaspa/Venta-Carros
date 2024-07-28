bb1f2a0e5f8d75dc4182670dc6411393c61ca77f
ccuaspa <ccuaspa@unal.edu.co>
2024-07-27 20:57:19 -0500

    modal update

--- js/index.js
+++ js/index.js
@@ -25,6 +25,44 @@ $(function(){
         console.log("El modal Reanult 4 se oculto")
     });
 
+    $('#masInfoDC').on('show.bs.modal', function(e){
+        console.log("El modal Dodge challenger se esta mostrando")
+        $('#masInfoDCBtn').removeClass('bg-primary');
+        $('#masInfoDCBtn').addClass('bg-dark');
+        $('#masInfoDCBtn').prop('disabled', true);
+    });
+    $('#masInfoDC').on('shown.bs.modal', function(e){
+        console.log("El modal Dodge challenger se mostro")
+    });
+    $('#masInfoDC').on('hide.bs.modal', function(e){
+        console.log("El modal Dodge challenger se oculta")
+        $('#masInfoDCBtn').removeClass('bg-dark');
+        $('#masInfoDCBtn').addClass('bg-primary');
+        $('#masInfoDCBtn').prop('disabled', false);
+    });
+    $('#masInfoDC').on('hidden.bs.modal', function(e){
+        console.log("El modal Dodge challenger se oculto")
+    });
+
+    $('#masInfoCar').on('show.bs.modal', function(e){
+        console.log("El modal mas info carro se esta mostrando")
+        $('#masInfoCarBtn').removeClass('bg-primary');
+        $('#masInfoCarBtn').addClass('bg-dark');
+        $('#masInfoCarBtn').prop('disabled', true);
+    });
+    $('#masInfoCar').on('shown.bs.modal', function(e){
+        console.log("El modal mas info carro se mostro")
+    });
+    $('#masInfoCar').on('hide.bs.modal', function(e){
+        console.log("El modal mas info carro se oculta")
+        $('#masInfoCarBtn').removeClass('bg-dark');
+        $('#masInfoCarBtn').addClass('bg-primary');
+        $('#masInfoCarBtn').prop('disabled', false);
+    });
+    $('#masInfoCar').on('hidden.bs.modal', function(e){
+        console.log("El modal mas info carro se oculto")
+    });
+
 
     $('#contacto').on('show.bs.modal', function(e){
         console.log("El modal contacto se esta mostrando")
